e72aa7ce45fb482a8828921d0455beca06c7a41b
Vyacheslav Krachkov <vskrachkov@gmail.com>
2017-11-26 00:31:08 +0200

    initial

---
+++ flask-app-src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from redis import Redis
+
+DEBUG = True
+
+REDIS = {
+    'LOCATION': [
+        ('redis', 6379)
+    ]
+}
+
+app = Flask(__name__)
+redis = Redis(host=REDIS['LOCATION'][0][0], port=REDIS['LOCATION'][0][1])
+
+@app.route('/')
+def index():
+    hits = redis.incr('hits')
+    return f'Hello from flask app: {hits} ###'
+
+@app.route('/<int:number>')
+def num(number):
+    return f'Your num is {number}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=DEBUG)
